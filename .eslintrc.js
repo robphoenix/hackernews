0e10db157caa40a40ade761503d5399100fdd150
Rob Phoenix <rob@robphoenix.com>
2018-04-13 21:16:57 +0100

    prettier & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
-import React, { Component } from "react"
-import "./App.css"
+import React, { Component } from 'react';
+import './App.css';
 
 const items = [
   {
-    title: "React",
-    url: "http://facebook.github.io/react/",
-    author: "Jordan Walke",
+    title: 'React',
+    url: 'http://facebook.github.io/react/',
+    author: 'Jordan Walke',
     num_comments: 3,
     points: 4,
-    objectID: 0
+    objectID: 0,
   },
   {
-    title: "Redux",
-    url: "http://github.com/reactjs/redux",
-    author: "Dan Abramov, Andrew Clark",
+    title: 'Redux',
+    url: 'http://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
     points: 5,
-    objectID: 1
-  }
-]
+    objectID: 1,
+  },
+];
 
-const Button = ({ onClick, className = "", children }) => (
+const Button = ({ onClick, className = '', children }) => (
   <button onClick={onClick} className={className} type="button">
     {children}
   </button>
-)
+);
 
 const Search = ({ value, onChange, children }) => (
   <form>
     {children}
     <input type="text" value={value} onChange={onChange} />
   </form>
-)
+);
 
 const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase())
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 const Table = ({ list, pattern, onDismiss }) => (
   <div className="table">
     {list.filter(isSearched(pattern)).map(item => (
       <div key={item.objectID} className="table-row">
-        <span style={{ width: "40%" }}>
+        <span style={{ width: '40%' }}>
           <a href={item.url}>{item.title}</a>
         </span>
-        <span style={{ width: "30%" }}>{item.author}</span>
-        <span style={{ width: "10%" }}>{item.num_comments}</span>
-        <span style={{ width: "10%" }}>{item.points}</span>
-        <span style={{ width: "10%" }}>
+        <span style={{ width: '30%' }}>{item.author}</span>
+        <span style={{ width: '10%' }}>{item.num_comments}</span>
+        <span style={{ width: '10%' }}>{item.points}</span>
+        <span style={{ width: '10%' }}>
           <Button
             onClick={() => onDismiss(item.objectID)}
             className="button-inline"
@@ -57,32 +57,32 @@ const Table = ({ list, pattern, onDismiss }) => (
       </div>
     ))}
   </div>
-)
+);
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       list: items,
-      searchTerm: ""
-    }
+      searchTerm: '',
+    };
 
-    this.onSearchChange = this.onSearchChange.bind(this)
-    this.onDismiss = this.onDismiss.bind(this)
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
   }
 
   onSearchChange(e) {
-    this.setState({ searchTerm: e.target.value })
+    this.setState({ searchTerm: e.target.value });
   }
 
   onDismiss(id) {
-    const updatedList = this.state.list.filter(item => item.objectID !== id)
-    this.setState({ list: updatedList })
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
   }
 
   render() {
-    const { searchTerm, list } = this.state
+    const { searchTerm, list } = this.state;
     return (
       <div className="page">
         <div className="interactions">
@@ -92,8 +92,8 @@ class App extends Component {
         </div>
         <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
