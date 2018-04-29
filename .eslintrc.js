b76ff4c1e274d874fa2214ed6f67f6326301890a
Rob Phoenix <rob@robphoenix.com>
2018-04-29 08:17:38 +0100

    Add further snapshot tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   extends: 'airbnb',
+  plugins: ['prettier'],
   rules: {
     'react/jsx-filename-extension': 'off',
     'react/prop-types': 'off',
     'react/no-unused-state': 'off',
     'react/no-did-mount-set-state': 'off',
-    'jsx-a11y/no-hash-href': 'off'
+    'jsx-a11y/no-hash-href': 'off',
+    'object-curly-newline': 'off'
   }
 };
